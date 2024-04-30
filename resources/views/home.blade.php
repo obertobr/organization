2d2f668e57bf0dc77d83532024093da46b7d5818
obertobr <real.azzi@gmail.com>
2024-04-30 03:35:33 -0300

    home page and controllers added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('content')
+
+<h1>home</home>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])-> name('home');
+Route::get('/items', [ItemController::class, 'index'])-> name('items.index');
+Route::get('/items/create', [ItemController::class, 'create'])-> name('items.create');
+Route::get('/items', [ItemController::class, 'store'])-> name('items.store');
+Route::get('/items/{item}', [ItemController::class, 'show'])-> name('items.show');
+Route::get('/items/{item}/edit', [ItemController::class, 'edit'])-> name('items.edit');
+Route::get('/items/{item}', [ItemController::class, 'update'])-> name('items.update');
+Route::get('/items/{item}', [ItemController::class, 'destroy'])-> name('items.destroy');

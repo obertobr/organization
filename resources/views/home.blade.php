4104afa33aa31a671d9774ff68060e8062aba759
obertobr <real.azzi@gmail.com>
2024-05-30 01:37:20 -0300

    button to add item on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,9 @@
 
 <header>
     <input id="search" type="search" autocomplete="off" />
+    <a id="create" href="{{ route('items.create')}}">
+        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>
+    </a>
     <input id="filter" type="button" value="Filtros"/>
 </header>
 <div id="tags">
